238ab00d7dfc471aa8d1a6fcdc51f54034b72e63
Sachie Sharma <sachie@mashou.local>
2021-06-21 22:47:55 -0400

    added hello to paper

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Sachie from './Sachie';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Sachie/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Livre Journal! :)
         </p>
         <a
           className="App-link"
---
+++ src/Sachie.js
@@ -0,0 +1,5 @@
+import Paper from '@material-ui/core/Paper';
+export default function Sachie (){
+    return(<Paper>hello</Paper>)
+
+}
