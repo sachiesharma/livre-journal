15bf0b484c3ee80879a9d71a213f1baf26a10a69
Sachie Sharma <sachie@mashou.local>
2021-06-27 21:13:32 -0400

    added button to create journal entry

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Sachie from './Sachie';
+import logo from "./logo.svg";
+import "./App.css";
+import Sachie from "./Sachie";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Sachie/>
+        <Sachie />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Livre Journal! :)
-        </p>
+        <p>Welcome to Livre Journal! :)</p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/Sachie.js
+++ src/Sachie.js
@@ -1,5 +1,9 @@
-import Paper from '@material-ui/core/Paper';
-export default function Sachie (){
-    return(<Paper>hello</Paper>)
-
+import { Button } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+export default function Sachie() {
+  return (
+    <Paper>
+      <Button variant="contained">Create Entry</Button>
+    </Paper>
+  );
 }
