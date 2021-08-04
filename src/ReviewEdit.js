373f55fdc026ea55c93119c1d5a6019bd377b765
Sachie Sharma <sachie@gmail.com>
2021-08-03 21:34:41 -0400

    added character and writing ratings

--- src/ReviewEdit.js
+++ src/ReviewEdit.js
@@ -2,8 +2,11 @@ import { Box, Fab, Grid, TextField, makeStyles } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import { useFirestore, useFirestoreDocData, useUser } from "reactfire";
 import { useHistory, useParams } from "react-router-dom";
-
+import { withStyles } from "@material-ui/core/styles";
+import Rating from "@material-ui/lab/Rating";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 import DoneIcon from "@material-ui/icons/Done";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   textField: {
@@ -14,6 +17,15 @@ const useStyles = makeStyles((theme) => ({
     bottom: theme.spacing(2),
   },
 }));
+
+const StyledRating = withStyles({
+  iconFilled: {
+    color: "#dda0dd",
+  },
+  iconHover: {
+    color: "#e576e5",
+  },
+})(Rating);
 
 export default function ReviewEdit() {
   const classes = useStyles();
@@ -32,9 +44,13 @@ export default function ReviewEdit() {
   });
 
   const [content, setContent] = useState("");
+  const [characterRating, setCharacterRating] = useState("");
+  const [writingRating, setWritingRating] = useState("");
 
   useEffect(() => {
     setContent(entry?.content || "");
+    setCharacterRating(entry?.characterRating);
+    setWritingRating(entry?.writingRating);
   }, [entry]);
 
   const updateContent = (event) => {
@@ -42,7 +58,7 @@ export default function ReviewEdit() {
   };
 
   const updateFirestoreContent = () => {
-    const payload = { content };
+    const payload = { content, characterRating, writingRating };
     if (!entry?.date) {
       payload.date = new Date();
     }
@@ -58,6 +74,26 @@ export default function ReviewEdit() {
         placeholder="Book title"
         value={content}
         onChange={updateContent}
+      />
+      <Typography component="legend">Characters</Typography>
+      <StyledRating
+        name="character-rating"
+        precision={0.5}
+        icon={<FavoriteIcon fontSize="inherit" />}
+        value={characterRating}
+        onChange={(event, newRating) => {
+          setCharacterRating(newRating);
+        }}
+      />
+      <Typography component="legend">Writing</Typography>
+      <StyledRating
+        name="writing-rating"
+        precision={0.5}
+        icon={<FavoriteIcon fontSize="inherit" />}
+        value={writingRating}
+        onChange={(event, newRating) => {
+          setWritingRating(newRating);
+        }}
       />
       <Grid container justifyContent="flex-end">
         <Fab
